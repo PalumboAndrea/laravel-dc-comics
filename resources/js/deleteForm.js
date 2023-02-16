5acd461f4b7500e5182cec43ec3c070977cac46f
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-02-16 17:50:17 +0100

    correct syntax

--- resources/js/deleteForm.js
+++ resources/js/deleteForm.js
@@ -4,7 +4,8 @@ deleteForms.forEach((element) => {
     element.addEventListener('submit', function (event){
         event.preventDefault();
         const formElementName = element.getAttribute('data-element-name');
-        const confirmPopUp = window.confirm(`Are you sure to delete ${{formElementName}}?`);
+        console.log(formElementName)
+        const confirmPopUp = window.confirm(`Are you sure to delete ${formElementName}?`);
         if (confirmPopUp) this.submit();
     })
 })
